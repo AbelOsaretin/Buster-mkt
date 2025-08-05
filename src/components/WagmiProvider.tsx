a0fb59f95458e6532f435b1a7c4969b93dc918fa
0xedev <oxdeve@gmail.com>
2025-08-05 09:00:21 +0100

    bug fixed

--- src/components/WagmiProvider.tsx
+++ src/components/WagmiProvider.tsx
@@ -3,10 +3,10 @@
 import React from "react"; // Import React
 import { WagmiConfig, createConfig, http } from "wagmi";
 import { base } from "wagmi/chains";
-import { farcasterFrame } from "@farcaster/frame-wagmi-connector";
+import { farcasterFrame } from "@farcaster/miniapp-wagmi-connector";
 import { metaMask } from "wagmi/connectors";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+// import { sdk } from "@farcaster/miniapp-sdk";
 const wagmiConfig = createConfig({
   chains: [base],
   transports: { [base.id]: http(process.env.NEXT_PUBLIC_ALCHEMY_RPC_URL) },
@@ -14,7 +14,7 @@ const wagmiConfig = createConfig({
     farcasterFrame(),
     metaMask({
       dappMetadata: {
-        name: "Buster Market",
+        name: "Policast",
         url: typeof window !== "undefined" ? window.location.origin : "",
       },
     }),
